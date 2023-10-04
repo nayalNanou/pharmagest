5b86d0104b746709077e73206216cdffe3f8726f
nayalNanou <nayal.christophcolomb@gmail.com>
2023-10-05 08:55:01 +1300

    Amélioration du style de la view login

---
+++ src/main/java/view/login/FormField.java
@@ -0,0 +1,65 @@
+package view.login;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FormField {
+	private JPanel panel;
+	private JLabel label;
+	private JTextField textField;
+	private GridBagConstraints labelStyle;
+	private GridBagConstraints textFieldStyle;
+	
+	public JPanel getPanel()
+	{
+		return this.panel;
+	}
+	
+	public JTextField getTextField()
+	{
+		return this.textField;
+	}
+	
+	public FormField(String labelText)
+	{
+		this.instantiateComponents(labelText);
+		this.styleComponents();
+		this.addComponents();
+	}
+	
+	public void instantiateComponents(String labelText)
+	{
+		this.panel = new JPanel(new GridBagLayout());
+		this.label = new JLabel(labelText);
+		this.textField = new JTextField("");		
+		this.labelStyle = new GridBagConstraints();
+		this.textFieldStyle = new GridBagConstraints();
+	}
+	
+	public void styleComponents()
+	{
+		this.label.setFont(new Font("Arial", Font.PLAIN, 18));
+		this.label.setBorder(BorderFactory.createEmptyBorder(0, 0, 4, 0));
+		this.textField.setColumns(20);
+		this.textField.setFont(new Font("Arial", Font.PLAIN, 20));
+		
+		this.textField.setBorder(
+			BorderFactory.createCompoundBorder(
+	        	this.textField.getBorder(), 
+	        	BorderFactory.createEmptyBorder(5, 5, 5, 5)
+	        )
+        );
+		
+		this.labelStyle.gridy = 0;
+		this.labelStyle.anchor = GridBagConstraints.WEST;
+		this.textFieldStyle.gridy = 1;
+		
+		this.panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	}
+	
+	public void addComponents()
+	{
+		panel.add(this.label, this.labelStyle);
+		panel.add(this.textField, this.textFieldStyle);
+	}
+}
--- src/main/java/view/login/PanelLoginForm.java
+++ src/main/java/view/login/PanelLoginForm.java
@@ -8,22 +8,33 @@ import java.awt.*;
 import controller.LoginController;
 
 public class PanelLoginForm {
+	private JLabel appTitle;
 	private JPanel panel;
-	private LoginField loginField;
-	private PasswordField passwordField;
+	private JPanel topBlock;
+	private JPanel bottomBlock;
+	private FormField loginField;
+	private FormField passwordField;
 	private JButton loginButton;
+	
+	private GridBagConstraints appTitleStyle;
+	private GridBagConstraints loginFieldStyle;
+	private GridBagConstraints passwordFieldStyle;
+	private GridBagConstraints loginButtonStyle;
+	
+	private GridBagConstraints topBlockStyle;
+	private GridBagConstraints bottomBlockStyle;
 	
 	public JPanel getPanel()
 	{
 		return this.panel;
 	}
 	
-	public LoginField getLoginField()
+	public FormField getLoginField()
 	{
 		return this.loginField;
 	}
 	
-	public PasswordField getPasswordField()
+	public FormField getPasswordField()
 	{
 		return this.passwordField;
 	}
@@ -31,40 +42,69 @@ public class PanelLoginForm {
 	public PanelLoginForm()
 	{
 		this.instantiateComponents();
+		this.implementLogic();
 		this.styleComponents();
 		this.addComponents();
 	}
 	
 	private void instantiateComponents()
 	{
+		this.appTitle = new JLabel("PharmaGest");
 		this.panel = new JPanel(new GridBagLayout());
-		this.loginField = new LoginField();
-		this.passwordField = new PasswordField();
+		this.topBlock = new JPanel(new GridBagLayout());
+		this.bottomBlock = new JPanel(new GridBagLayout());
+		this.loginField = new FormField("Nom d'utilisateur");
+		this.passwordField = new FormField("Mot de passe");
 		this.loginButton = new JButton("Se connecter");
 		
+		this.appTitleStyle = new GridBagConstraints();
+		this.loginFieldStyle = new GridBagConstraints();
+		this.passwordFieldStyle = new GridBagConstraints();
+		this.loginButtonStyle = new GridBagConstraints();
+		
+		this.topBlockStyle = new GridBagConstraints();
+		this.bottomBlockStyle = new GridBagConstraints();
+	}
+	
+	private void implementLogic()
+	{
 		loginButton.addActionListener(LoginController.signIn);
 	}
 	
 	private void styleComponents()
 	{
-		this.panel.setBorder(this.getPanelPadding());
+		this.panel.setBorder(BorderFactory.createEmptyBorder(60, 60, 60, 60));
+
+		this.appTitle.setFont(new Font("Arial", Font.PLAIN, 30));
+		this.appTitle.setBorder(BorderFactory.createEmptyBorder(0, 0, 40, 0));
+		
+		this.loginButton.setFont(new Font("Arial", Font.PLAIN, 18));
+		this.loginButton.setBorder(
+			BorderFactory.createCompoundBorder(
+				this.loginButton.getBorder(),
+				BorderFactory.createEmptyBorder(5, 40, 5, 40)
+			)
+		);
+		
+		this.bottomBlock.setBorder(BorderFactory.createEmptyBorder(14, 0, 0, 0));
+		
+		this.topBlockStyle.gridy = 0;
+		this.bottomBlockStyle.gridy = 1;
+		
+		this.appTitleStyle.gridy = 0;
+		this.loginFieldStyle.gridy = 1;
+		this.passwordFieldStyle.gridy = 2;
+		
+		this.loginButtonStyle.gridy = 2;
 	}
 	
 	private void addComponents()
 	{
-		GridBagConstraints gbc = new GridBagConstraints();
-		
-		gbc.gridy = 0;
-		this.panel.add(loginField.getPanel(), gbc);
-		gbc.gridy = 1;
-		this.panel.add(passwordField.getPanel(), gbc);
-		gbc.gridy = 2;
-		this.panel.add(this.loginButton, gbc);
-	}
-	
-	private Border getPanelPadding()
-	{
-		Border panelPadding = BorderFactory.createEmptyBorder(30, 20, 20, 30);
-		return panelPadding;
+		this.topBlock.add(this.appTitle, this.appTitleStyle);
+		this.topBlock.add(this.loginField.getPanel(), this.loginFieldStyle);
+		this.topBlock.add(this.passwordField.getPanel(), this.passwordFieldStyle);
+		this.bottomBlock.add(this.loginButton, this.loginButtonStyle);
+		this.panel.add(this.topBlock, this.topBlockStyle);
+		this.panel.add(this.bottomBlock, this.bottomBlockStyle);
 	}
 }
